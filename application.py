b3b82265ca211e6aaa8d255351b6fe6fe29128cb
Sohaila <sohailaelbasel@gmail.com>
2020-01-01 17:29:02 +0200

    html and css files for index and layout

--- application.py
+++ application.py
@@ -1,13 +1,21 @@
 import os
 
-from flask import Flask
+from flask import Flask, session, render_template, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
-
-@app.route("/")
+@app.route("/", methods = ['POST', 'GET'])
 def index():
-    return "Project 2: TODO"
+    if request.method == 'POST':
+        check_name = request.form.get('displayname')
+        if check_name.isspace() or check_name == '':
+            message = "Invalid name"
+            return render_template('index.html',message = message)
+    return render_template('index.html')
+
+
+if __name__ =='__main__':
+    app.run(debug=True)
