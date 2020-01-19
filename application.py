67e5d96ec759e07cc83178044df4383164d024b6
sohaila-elbasel <sohailaelbasel@hotmail.com>
2020-01-19 16:05:24 +0200

    display name with javascript

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session, render_template, request
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
