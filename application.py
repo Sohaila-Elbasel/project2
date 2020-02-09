017cd5a4609ea6468c5f567f0da10d18e4009a58
sohaila-elbasel <sohailaelbasel@hotmail.com>
2020-02-09 14:56:25 +0200

    Create channel javaScript without submit

--- application.py
+++ application.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+channels = {"General": ["Sohaila"]}
+
 @app.route("/", methods = ['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        check_name = request.form.get('displayname')
-        if check_name.isspace() or check_name == '':
-            message = "Invalid name"
-            return render_template('index.html',message = message)
+        return render_template('index.html')
     return render_template('index.html')
+
 
 
 if __name__ =='__main__':
